2dc9623c4503c0a4059b92f1ce30003493988f85
Daniel Perwejnis <daniel.perwejnis@gmail.com>
2017-04-09 17:22:50 +0200

    added Fuzzy Regules Set classes

--- MamdaniFuzzySystem/FuzzyReguleSet.cs
+++ MamdaniFuzzySystem/FuzzyReguleSet.cs
@@ -1,26 +1,25 @@
-﻿using System;
+﻿#region Usings
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#endregion    
+
 namespace MamdaniFuzzySystem
 {
-    public class FuzzyReguleSet<T>
+    public class FuzzyReguleSet
     {
         #region Properties
 
-        
+        public List<FuzzyValue> FuzzyRegulesList { get; set }
 
         #endregion
-        public Dictionary<T,float> RegulesSet { get; set; }
 
         #region Public Methods
 
-        public FuzzyReguleSet()
-        {
-            RegulesSet = new Dictionary<T, float>();
-        }
 
         #endregion
 
---
+++ MamdaniFuzzySystem/FuzzyValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MamdaniFuzzySystem
+{
+    public class FuzzyValue
+    {
+        #region Properties
+
+      
+
+        public string FuzzyValueName { get; set; }
+
+        public float CrispValueMin { get; set; }
+
+        public float CrispValueMax { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public FuzzyValue(string fuzzyValueName, float crispValueMin, float crispValueMax)
+        {
+            FuzzyValueName = fuzzyValueName;
+            CrispValueMin = crispValueMin;
+            CrispValueMax = crispValueMax;
+        }
+
+        #endregion
+    }
+
+
+}
