2eb247006148d160993462358aacedde492cd2ce
Daniel Perwejnis <daniel.perwejnis@gmail.com>
2017-04-30 00:23:42 +0200

    added  AHP_Tools class.

---
+++ MathTools/AHP_Tools.cs
@@ -0,0 +1,57 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace MathTools
+{
+    public class AHP_Tools
+    {
+
+        #region Private Fields
+
+        private float[,] InputWeights { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public AHP_Tools(float[,] inputWeights)
+        {
+            this.InputWeights = inputWeights;
+
+           
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public float[] GetOutputWeights()
+        {
+            var costam = InputWeights;
+            for (int i = 0; i < Math.Sqrt(costam.Length); i++)
+            {
+                costam = Normalize(costam);
+            }
+            return null;
+        }
+
+        private static float[,] Normalize(float[,] inputArray)
+        {
+            return inputArray;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}  
