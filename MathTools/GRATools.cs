7579ab334025475424629306867d85fd481b978f
Daniel Perwejnis <daniel.perwejnis@gmail.com>
2017-06-04 16:05:17 +0200

    Added documentation headers for normalization methods.

--- MathTools/GRATools.cs
+++ MathTools/GRATools.cs
@@ -13,15 +13,44 @@ namespace MathTools
 {
     public static class GRATools
     {
+        #region Public Methods
+
+        /// <summary>
+        /// Normalizes passed value, by smaller the better pattern.
+        /// </summary>
+        /// <param name="passedValues">
+        /// Passed values collection.
+        /// </param>
+        /// <param name="value">
+        /// Value to normalize.
+        /// </param>
+        /// <returns>
+        /// <see cref="float"/>
+        /// Normalized value.
+        /// </returns>
         public static float NormalizateSmallerTheBetter(IList<float> passedValues, float value)
         {
-            return (passedValues.Max() - value)/(passedValues.Max() - passedValues.Min());
+            return (passedValues.Max() - value) / (passedValues.Max() - passedValues.Min());
         }
 
+        /// <summary>
+        /// Normalizes passed value, by larger the better pattern.
+        /// </summary>
+        /// <param name="passedValues">
+        /// Passed values collecton.
+        /// </param>
+        /// <param name="value"></param>
+        /// <returns>
+        /// <see cref="float"/>
+        /// Normalized value.
+        /// </returns>
         public static float NormalizeLargerTheBetter(IList<float> passedValues, float value)
         {
-            return (value - passedValues.Min())/(passedValues.Max() - passedValues.Min());
+            return (value - passedValues.Min()) / (passedValues.Max() - passedValues.Min());
         }
+
+        #endregion
+
     }
 
 }
