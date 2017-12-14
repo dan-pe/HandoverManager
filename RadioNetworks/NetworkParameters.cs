59589a6cc159ae0a2f59559c14ebcaec5473a5c6
Daniel Perwejnis <daniel.perwejnis@gmail.com>
2017-12-14 07:22:25 +0100

    Added Custom Attribute for Network Parameters

--- RadioNetworks/NetworkParameters.cs
+++ RadioNetworks/NetworkParameters.cs
@@ -1,19 +1,47 @@
 namespace RadioNetworks
 {
+    #region Usings
+
+    using System;
+
+    #endregion
+
+
     public class NetworkParameters
     {
         #region Properties
 
+        [NetworkMetric]
         public double ThroughputInMbps { get; set; }
+
+        [NetworkMetric]
         public double BitErrorRate { get; set; }
+
+        [NetworkMetric]
         public double BurstErrorRate { get; set; }
+
+        [NetworkMetric]
         public double PacketLossPercentage { get; set; }
+
+        [NetworkMetric]
         public double DelayInMsec { get; set; }
+
+        [NetworkMetric]
         public double ResponseTimeInMsec { get; set; }
+
+        [NetworkMetric]
         public double JitterInMsec { get; set; }
+
+        [NetworkMetric]
         public double SecurityLevel { get; set; }
+
+        [NetworkMetric]
         public double CostInUnitsPerByte { get; set; }
 
         #endregion
+    }
+
+    public class NetworkMetricAttribute : Attribute
+    {
     }
 }
