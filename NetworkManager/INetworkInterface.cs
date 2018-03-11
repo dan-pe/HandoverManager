e3fa4c53f0b0ba348a551db8d15f96a9fe4cb4ca
Daniel Perwejnis <daniel.perwejnis@gmail.com>
2018-03-11 23:30:42 +0100

    base network for network interfaces

---
+++ NetworkManager/INetworkInterface.cs
@@ -0,0 +1,17 @@
+﻿namespace NetworkManager
+{
+    public abstract class NetworkInterface
+    {
+        protected InterfaceType InterfaceType;
+
+        protected string InterfaceName;
+
+        protected int InterfaceSpeed;
+    }
+
+    public enum InterfaceType
+    {
+        Wifi,
+        RDI
+    }
+}
